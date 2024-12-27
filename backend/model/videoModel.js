f7e7c1169107e20f4468156e0eb3f7fdc9c12067
irakeshbind <rakeshbind351998@gmail.com>
2024-12-27 22:26:38 +0530

    create model for video upload

---
+++ backend/model/videoModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const videoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = mongoose.model("Video", videoSchema);
+
+module.exports = Video;
